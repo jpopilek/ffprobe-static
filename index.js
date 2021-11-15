9634e28016b386ed2d07ee4dada5d76abfc4087e
John Popilek <john.popilek@gmail.com>
2021-11-15 16:06:48 -0600

    Adding support for m1 macbook

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 //
 // With credits to https://github.com/eugeneware/ffmpeg-static
 //
-var os = require('os');
-var path = require('path');
+var os = require("os");
+var path = require("path");
 
 var platform = os.platform();
-if (platform !== 'darwin' && platform !=='linux' && platform !== 'win32') {
-  console.error('Unsupported platform.');
+if (platform !== "darwin" && platform !== "linux" && platform !== "win32") {
+  console.error("Unsupported platform.");
   process.exit(1);
 }
 
 var arch = os.arch();
-if (platform === 'darwin' && arch !== 'x64') {
-  console.error('Unsupported architecture.');
+if (platform === "darwin" && arch !== "x64" && arch !== "arm64") {
+  console.error("Unsupported architecture.");
   process.exit(1);
 }
 
 var ffprobePath = path.join(
   __dirname,
-  'bin',
+  "bin",
   platform,
   arch,
-  platform === 'win32' ? 'ffprobe.exe' : 'ffprobe'
+  platform === "win32" ? "ffprobe.exe" : "ffprobe"
 );
 
 exports.path = ffprobePath;
